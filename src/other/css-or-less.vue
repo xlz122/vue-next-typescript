da19a28b1258ad764c1ccbfae6409fefbd662ca4
xlz122 <122874722@qq.com>
2020-10-16 15:18:22 +0800

    css/less全局变量使用，简单实现jq语法

---
+++ src/other/css-or-less.vue
@@ -0,0 +1,14 @@
+<template>
+  <p class="text">测试css和less全局变量使用上的不同</p>
+  <p>css 不需要单独引入文件</p>
+  <p>less 需要单独引入文件</p>
+</template>
+
+<style lang="less" scoped>
+@import '../index.less';
+
+.text {
+  width: @sidebar;
+  color: var(--Color);
+}
+</style>
---
+++ src/other/jq.ts
@@ -0,0 +1,15 @@
+class Jq {
+  constructor(arg: any) {
+    this.element = document.querySelector(arg);
+  }
+  
+  element: null;
+
+  click(fn: any): void {
+    (this.element as any).addEventListener("click", fn);
+  }
+}
+
+export default function $(arg: any) {
+  return new Jq(arg);
+}
