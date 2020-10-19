b0eccb459edb9f4c8be01ac7eb344d39dc8e2ef2
xlz122 <122874722@qq.com>
2020-10-19 11:37:39 +0800

    代码整理，toRefs,toRef

---
+++ src/vue-new-api/toRefs.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>toRefs包裹的值，解构并且能够保持响应性。</p>
+  <p>{{ name }}</p>
+  <p>{{ age }}</p>
+  <p>{{ sex }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from 'vue';
+
+interface State {
+  name: string;
+  age: number;
+  sex?: string;
+}
+
+export default defineComponent({
+  setup() {
+    const state = reactive<State>({
+      name: 'xyc',
+      age: 25,
+      sex: '男'
+    });
+
+    return {
+      ...toRefs(state)
+    };
+  }
+});
+</script>
