c707e431f9a2a32c1e05ec0ee4bd45886518ce47
xlz122 <122874722@qq.com>
2020-11-02 11:34:25 +0800

    更新路由 vuex版本

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,3 +1,21 @@
 <template>
   <p></p>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    function fn() {
+      return new Promise((resolve, reject) => {
+        resolve('回调');
+      })
+    }
+    fn()
+      .then(res => {
+        console.log(res);
+      })
+  }
+});
+</script>
