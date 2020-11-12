a2403abc9103af8eaf66a0456d3619f940285af1
xlz122 <122874722@qq.com>
2020-11-12 14:13:08 +0800

    简易数据ts定义方式，按钮权限控制使用自定义指令方式

---
+++ src/other/ArrayInterface.ts
@@ -0,0 +1,6 @@
+// 用接口表示数组
+interface NumberArray {
+  [index: number]: number
+}
+
+let numArr: NumberArray = [1, 2, 3];
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -3,19 +3,18 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   setup() {
     function fn() {
-      return new Promise((resolve, reject) => {
+      return new Promise(resolve => {
         resolve('回调');
-      })
+      });
     }
-    fn()
-      .then(res => {
-        console.log(res);
-      })
+    fn().then(res => {
+      console.log(res);
+    });
   }
 });
 </script>
