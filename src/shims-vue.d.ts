9e97bfce4cecbccdd2b4f29263ba186247cbc266
xlz122 <122874722@qq.com>
2021-03-16 09:52:39 +0800

    fix: window新增属性，导入lodash，ts检测爆红问题

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -7,3 +7,11 @@ declare module '*.vue' {
 
 // 解决.vue文件导入ts文件时，使用路径别名路径找不到问题
 declare module '*.ts';
+
+// window 新增属性，ts检测不到问题
+declare interface Window {
+  g: string
+}
+
+// 声明lodash
+declare module 'lodash';
