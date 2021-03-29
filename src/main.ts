38267748eff713467488814908125fb59d19f0a1
xlz122 <122874722@qq.com>
2021-03-29 17:21:11 +0800

    fix: 捕获错误函数增加备注

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,15 @@ import '@/index.less';
 const app = createApp(App);
 
 // 捕获全局错误
-// app.config.errorHandler = (err, vm, info) => {
-//   console.log(err);
-//   console.log(vm);
-//   console.log(info);
-// }
+// 错误一旦被这个方法捕获，就不会外抛到在控制台
+// 可以通过 console.log(err); 来抛出错误
+// 也可以通过 throw err 来向控制台抛出错误
+app.config.errorHandler = (err, vm, info) => {
+  console.log(err);
+  console.log(vm);
+  console.log(info);
+  throw err;
+}
 
 app.use(store);
 app.use(router);
