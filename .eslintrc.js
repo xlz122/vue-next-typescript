edc70eb522abe544133ba41094aa6c46ff535e7f
xlz122 <122874722@qq.com>
2021-06-17 10:40:40 +0800

    feat: 升级vue、vue-router、vuex版本

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended',
-    '@vue/prettier',
-    '@vue/prettier/@typescript-eslint'
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint",
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     // "@typescript-eslint/no-inferrable-types": "off", // 关闭类型推断
     '@typescript-eslint/no-explicit-any': 'off', // 关闭any类型时的警告
     '@typescript-eslint/no-use-before-define': [
@@ -23,7 +23,6 @@ module.exports = {
       { functions: false, classes: true, variables: true }
     ], // 禁止在变量定义之前使用它们 variables(变量)
     quotes: [2, 'single'], // 开启强制单引号
-    // 'max-len': [1, { code: 90 }] // 最大行长度
     'comma-dangle': [2, 'never'] // 对象和数组最后一项尾随逗号
-  }
+  },
 };
